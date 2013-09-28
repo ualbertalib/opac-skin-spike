580808f5eb9105edcabfe0778962d3741297a8a9
Sam Popowich <spopowich@gmail.com>
2013-09-28 11:36:37 -0230

    Break json results into individual records.

--- symphony2json.rb
+++ symphony2json.rb
@@ -1,11 +1,15 @@
 require "nokogiri"
 require "active_support/core_ext"
 require "json"
+require "pp"
 
-doc = Nokogiri::XML(File.open(ARGV[0]).read)
-json_results = Hash.from_xml(doc.to_xml).to_json
-of = File.open(ARGV[1], "w")
+json_results = []
+doc = Nokogiri::XML(File.open(ARGV[0]).read).remove_namespaces!
+doc.xpath("//HitlistTitleInfo").each do |record|
+  json_results << Hash.from_xml(record.to_xml).to_json
+end
 #replace this with direct ingest to ElasticSearch
+of = File.open(ARGV[1], "w")
 of.puts json_results
 of.close
 
